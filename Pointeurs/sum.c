4caa54bd267073bf748415357c9eb6a325d3ffdb
VM IUT <vm@iut>
2018-05-23 15:29:01 +0200

    fin ecriture sum

---
+++ Pointeurs/sum.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+
+double sum1 (double *tab, int size){
+double resutat=0;
+    for (int i=0; i<size ;++i){
+        
+	resultat= resultat + tab[i]
+    }
+return resultat;
+}
+
+double sum2 (double *tab, int size){
+
+double resutat=0;
+double *fin = tab +size;
+    for (double *p=tab ; p<fin ;++p){
+        
+	resultat= resultat + tab[p]
+    }
+return resultat;
+}
+
+
+}
+
