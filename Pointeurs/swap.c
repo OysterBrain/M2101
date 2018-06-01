a68cab247635b166b151e9a8ae4541a9f0da8c21
VM IUT <vm@iut>
2018-06-01 11:12:34 +0200

    fin swap2

--- Pointeurs/swap.c
+++ Pointeurs/swap.c
@@ -6,11 +6,20 @@ void swap (double x, double y) {
     y = z;
 }
 
+
+void swap2 (double *x, double *y) {
+    double z = *x;
+    *x = *y;
+    *y = z;
+}
+
 int main (void) {
     double a = 1;
     double b = 2;
     printf("a = %f et b = %f\n", a, b);
     swap(a, b);
+    printf("a = %f et b = %f\n", a, b);
+    swap2(&a,&b);
     printf("a = %f et b = %f\n", a, b);
     return 0;
 }
