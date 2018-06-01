b326d00b1522706613ef89c868a2ea98467f5b65
VM IUT <vm@iut>
2018-06-01 15:51:24 +0200

    fin tp5

--- Pointeurs/hello.c
+++ Pointeurs/hello.c
@@ -2,7 +2,19 @@
 
 void print_hello(){
 
-static int compteur=1;
+	static int compteur=1;
 
+	printf("Bonjour numéro %d \n",compteur);
+	compteur++;
+
+
+}
+
+void main(void){
+
+	for (int i=0;i<10;i++){
+
+		print_hello();
+	}
 
 }
