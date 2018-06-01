a2fc3f07004813a031f4a6d8420d3e167ea9bdd8
VM IUT <vm@iut>
2018-06-01 16:38:16 +0200

    fin exo1 tp6

--- Strings/strlen.c
+++ Strings/strlen.c
@@ -1,3 +1,20 @@
 #include <stdio.h>
 
+int mystrlen(const char *str){
+	int nbCar =0;
+	
+	while (*str != '\0'){
+		nbCar++;		
+		str++;
+			
+	}
+return nbCar;
+}
 
+int main(void){
+
+
+	char str[] ="Bonjour";
+	printf("longueur du mot :%d \n",mystrlen(str));
+
+}
