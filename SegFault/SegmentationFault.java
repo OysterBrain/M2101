301706b6f41e24b832eff81255365591d127c056
VM IUT <vm@iut>
2018-06-04 11:39:37 +0200

    fin 1.1 ,début 1.2 tp7

---
+++ SegFault/SegmentationFault.java
@@ -0,0 +1,10 @@
+
+public class SegmentationFault {
+
+	public static void main(String[] args){
+
+		int[] t =new int[10];
+		t[10]=1;
+	}	
+
+}
---
+++ SegFault/segmentation_fault.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+int main(void){
+	
+	int t[10];	
+	t[999]=1;
+	
+	
+
+
+
+
+
+}
