10e2ab4a5e52e49ce28b7a57e929455233ecb4de
VM IUT <vm@iut>
2018-06-08 11:27:28 +0200

    fin exo 1.3 et 2.1

---
+++ Leak/memory_leak.c
@@ -0,0 +1,19 @@
+#include <stdlib.h>
+#include <stdio.h>
+#include <unistd.h>
+
+int main (void) {
+    void *p;
+    for (int i = 0; i < 500000; ++i) {
+        p = malloc(0x80000000);
+        printf("p = %p\n", p);
+        if (p == 0) {
+            printf("malloc renvoie 0.\n");
+            exit(1);
+        }
+    free(p);
+    p=NULL;
+    }
+    return 0;
+}
+
